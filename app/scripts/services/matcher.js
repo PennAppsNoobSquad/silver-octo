4d73f016c57b799bf1f24c8924edd2b1dd1c519c
Lucas Yan <lucas@peer.com>
2016-01-23 18:12:48 -0500

    Added matcher

--- app/scripts/services/matcher.js
+++ app/scripts/services/matcher.js
@@ -4,20 +4,50 @@
  * @ngdoc service
  * @name silverOctoTestApp.matcher
  * @description
- * # matcher
+ * # Matches commands to actions
  * Factory in the silverOctoTestApp.
  */
 angular.module('silverOctoTestApp')
-  .factory('matcher', function () {
+  .factory('matcher', function ($log) {
     // Service logic
-    // ...
-
-    var meaningOfLife = 42;
+    var matchers = [
+        {
+            matcher: /^what.*?[medicines?|medications?|prescriptions?].*?[take|have][ ?](.*?)\??$/i,
+            handler: function(time) {
+                $log.debug("User is asking what medications to take", time);
+            }
+        },
+        {
+            matcher: /^when.*?i.*?take[ ?].*?(.*)\??$/i,
+            handler: function(medication) {
+                $log.debug("User is asking when he needs to take", medication);
+            }
+        },
+        {
+            matcher: /^i.*?took ?(.*)/i,
+            handler: function(medication) {
+                $log.debug("User took", medication);
+            }
+        },
+        {
+            matcher: /^.*?[tell|know].*?about[ ?](.*)\??/i,
+            handler: function(medication) {
+                $log.debug("User wants to know about", medication);
+            }
+        }
+    ];
 
     // Public API here
     return {
-      someMethod: function () {
-        return meaningOfLife;
+      match: function (string) {
+        string = string.toLowerCase();
+        string = string.replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ");
+        for (var i = 0; i < matchers.length; i++) {
+            var match = matchers[i].matcher.exec(string);
+            if(match) {
+                matchers[i].handler.apply(null, match.splice(1));
+            }
+        }
       }
     };
   });
