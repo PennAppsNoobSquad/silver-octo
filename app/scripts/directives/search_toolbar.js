3def0610a17d2cea413ba54917582a4c7f73bf93
Justin <me@justinmaslin.com>
2016-01-23 17:45:51 -0500

    Toggle button if user logged in

--- app/scripts/directives/search_toolbar.js
+++ app/scripts/directives/search_toolbar.js
@@ -7,12 +7,14 @@
  * # searchToolbar
  */
 angular.module('silverOctoTestApp')
-  .directive('searchToolbar', function (houndify) {
+  .directive('searchToolbar', function ($log, houndify, Auth, Ref, $firebaseObject) {
     return {
       templateUrl: 'views/search_toolbar.html',
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
-        scope.voiceSearchInProgress = false;
+        Auth.$requireAuth().then(function (user) {
+          scope.user = $firebaseObject(Ref.child('users/'+user.uid));
+        });
 
         scope.toggleVoiceSearch = function toggleVoiceSearch() {
           scope.voiceSearchInProgress = houndify.toggleVoiceSearch();
