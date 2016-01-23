c3499e1b7bb62048d1941454a963ba69799d58b4
Justin <me@justinmaslin.com>
2016-01-23 16:21:47 -0500

    Start integration with hound

--- app/scripts/directives/search_toolbar.js
+++ app/scripts/directives/search_toolbar.js
@@ -7,12 +7,20 @@
  * # searchToolbar
  */
 angular.module('silverOctoTestApp')
-  .directive('searchToolbar', function () {
+  .directive('searchToolbar', function (houndify) {
     return {
       templateUrl: 'views/search_toolbar.html',
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
+        scope.voiceSearchInProgress = false;
 
+        scope.toggleVoiceSearch = function toggleVoiceSearch() {
+          scope.voiceSearchInProgress = houndify.toggleVoiceSearch();
+        };
+
+        scope.textSearch = function textSearch(search) {
+          houndify.textSearch(search);
+        };
       }
     };
   });
